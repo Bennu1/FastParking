f84762bee5dbff9bba94439f590b09ffea678d28
Sebastian <juans.rianor@utadeo.edu.co>
2018-10-15 13:55:55 -0500

    nav2

--- app/src/main/java/com/example/sebastian/fastparking/Extender.java
+++ app/src/main/java/com/example/sebastian/fastparking/Extender.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Reservar.OnFragmentInteractionListener} interface
+ * {@link Extender.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Reservar#newInstance} factory method to
+ * Use the {@link Extender#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Reservar extends Fragment {
+public class Extender extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class Reservar extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Reservar() {
+    public Extender() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class Reservar extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Reservar.
+     * @return A new instance of fragment Extender.
      */
     // TODO: Rename and change types and number of parameters
-    public static Reservar newInstance(String param1, String param2) {
-        Reservar fragment = new Reservar();
+    public static Extender newInstance(String param1, String param2) {
+        Extender fragment = new Extender();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class Reservar extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reservar, container, false);
+        return inflater.inflate(R.layout.fragment_extender, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/sebastian/fastparking/MenuInferior.java
+++ app/src/main/java/com/example/sebastian/fastparking/MenuInferior.java
@@ -3,6 +3,8 @@ package com.example.sebastian.fastparking;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
@@ -16,6 +18,10 @@ public class MenuInferior extends AppCompatActivity {
 
     private BottomNavigationView mMainNav;
     private FrameLayout mMainFrame;
+
+    private Cartera carteraFragment;
+    private Reservar reservarFragment;
+    private Extender extenderFreame;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -46,9 +52,54 @@ public class MenuInferior extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_inferior);
 
-        mMainFrame = (FrameLayout) findViewById(R.id.btn_cartera);
-        mMainNav = (BottomNavigationView) findViewById(R.id.barraNav);
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
 
+        reservarFragment = new Reservar();
+        carteraFragment = new Cartera();
+        extenderFreame= new Extender();
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.btn_saliendo:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+
+                        return true;
+
+                    case R.id.btn_reserva:
+                        mMainNav.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment (reservarFragment);
+
+                        return true;
+                    case R.id.btn_cartera:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimaryDark);
+                        setFragment(carteraFragment);
+
+                        return true;
+                    case R.id.btn_tiempo:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(extenderFreame);
+
+                        return true;
+
+                        default:
+                            return false;
+                }
+
+            }
+        });
+
+
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
 
     }
 
