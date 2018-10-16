8905f5e5187b8feaa7350710ca2b504e5bee9931
Sebastian <juans.rianor@utadeo.edu.co>
2018-10-16 00:13:31 -0500

    ff3

--- app/src/main/java/com/example/sebastian/fastparking/Lateral.java
+++ app/src/main/java/com/example/sebastian/fastparking/Lateral.java
@@ -40,6 +40,7 @@ public class Lateral extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
     }
 
     @Override
